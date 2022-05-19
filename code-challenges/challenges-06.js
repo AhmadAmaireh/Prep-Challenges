c6c6b85d5c10973f9891600f5d4d45e179b165ca
AhmadAmaireh <a_amayreh@hotmail.com>
2022-05-19 03:02:33 +0300

    Prep-Challenges

--- code-challenges/challenges-06.js
+++ code-challenges/challenges-06.js
@@ -1,36 +1,66 @@
 "use strict";
 
-const { findMax, sumNums, reverseArray } = require("../code-challenges/challenges-06");
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 01:
+//  Required:
+//
+//  Write a function that takes an array and returns the maximum value in the provided array
+//  without using build-in functions.
+//
+// Input: [12, 32, 22, 45, 78, 12, 50]
+// Output: 78
+//
 
+const findMax = (arr) => {
+  let max = 0;
+  // write your code here
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (element > max) {
+      max = element;
+    }
+  }
+  return max;
+};
+// -------------------------------------------------------------------------------------------------------
 
-describe("Challenge01", () => {
-  test("Find_Max", () => {
-    expect(findMax([12, 32, 22, 45, 78, 12, 50])).toStrictEqual(78);
-    expect(findMax([20, 31, 15, 41, 20, 48])).toStrictEqual(48);
-    expect(findMax([88, 10, 10, 46, 71])).toStrictEqual(88);
-  });
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 02:
+//  Required:
+//
+//  Write a function that takes an arrays and returns the Sum of the numeric values
+//  inside the array (there could be strings inside), without using built-in functions
+//
+// Input: [20, '234', 'car', 41, 20, 'chair']
+// Output: 81
+//
 
-  test("Sum_Nums", () => {
-    expect(sumNums([78, "sad", 45, "hungry", "agony", 23, 10])).toStrictEqual(
-      156
-    );
-    expect(sumNums([20, "234", "car", 41, 20, "chair"])).toStrictEqual(81);
-    expect(sumNums([88, 10, 10, 46, 71])).toStrictEqual(225);
-  });
+const sumNums = (arr) => {
+  let sum = 0;
+  // write your code here
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == "number") {
+      sum += arr[i];
+    }
+  }
 
-  test.skip("reverse_Array", () => {
-    expect(reverseArray([1, 2, 3, 4, 5])).toStrictEqual([5, 4, 3, 2, 1]);
-    expect(reverseArray(["C#", "JS", "Ruby", "Python"])).toStrictEqual([
-      "Python",
-      "Ruby",
-      "JS",
-      "C#",
-    ]);
-    expect(reverseArray(["88", 10, "course", 71])).toStrictEqual([
-      71,
-      "course",
-      10,
-      "88",
-    ]);
-  });
-});
+  return sum;
+};
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 03:
+//  optional:
+//
+//  Write a function that takes an array and returns the array reversed
+//  without using built-in functions
+//
+// Input: ['C#', 'JS', 'Ruby','Python']
+// Output: ['Python','Ruby','JS','C#']
+
+const reverseArray = (arr) => {
+  // write your code here
+};
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { findMax, sumNums, reverseArray };
